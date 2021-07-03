cf6c224aeb3f4a9fb2cb999a03fc701abd49c809
Kamran999A <85412897+Kamran999A@users.noreply.github.com>
2021-07-03 19:14:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+
+            var str = Console.ReadLine();
+
+            var cls = new MyClass();
+
+            Func funcDell = cls.Space;
+            funcDell += cls.Reverse;
+
+            var run = new Run();
+
+            run.RunFunc(funcDell, str);
+
+        }
+    }
+}
---
+++ Run.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    public  class Run
+    {
+        public void RunFunc(Func func, string data)
+        {
+            func.Invoke(data);
+        }
+    }
+}
